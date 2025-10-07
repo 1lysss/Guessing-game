35a30a71872dd14a0ac446937a0858c7f3486a1e
Ilyass <irrifiilyass@gmail.com>
2025-10-07 15:23:41 +0100

    Added getInt() function

--- Guess.java
+++ Guess.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Guess {
     
-    public static final int RANGE = 100;
-
-    // this function is NOT STATIC on purpose
-    Scanner scanner = new Scanner(System.in);
+    public static final int RANGE = 20;
+    static Scanner scanner = new Scanner(System.in);
 
     public static int generated_num()
     {
@@ -13,33 +11,37 @@ public class Guess {
         return random;
     }
 
+    public static int getInt()
+    {
+        int input;
+        while(!scanner.hasNextInt())
+        {
+            System.out.print("Only accepts int, try again: ");
+            scanner.next();
+        }
+        input = scanner.nextInt();
+
+        return input;
+    }
+
     public static void main(String[] args)
     {
         int count = 0;
-        Guess user = new Guess();
         
         int random = generated_num(); 
         int guess = -1;
         
+        System.out.print("Start guessing: ");
         while (guess != random)
         {
-            System.out.print("Guess the number: ");
-            if (user.scanner.hasNextInt())
-            {
-                count++;
-                guess = user.scanner.nextInt();
-                
-                if (guess > random) System.out.println("Lower!");
-                else if (guess < random) System.out.println("Higher!");
-            }
-            else
-            {
-                System.out.println("Bad input! ");
-                user.scanner.next();
-            }
+            guess = getInt();
+         
+            if (guess > random) { System.out.println("Lower!"); count++; }
+            else if (guess < random) { System.out.println("Higher!"); count++; }
         }
 
         System.out.printf("Good guess! It was indeed: %d, guessed in %d attempts!\n", random, count);
     }
 }
+
 
